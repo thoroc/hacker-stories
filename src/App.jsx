a4c99578cb83f1a5e15a46851c94001c7c0df7b4
Thomas Roche <thomas.a.roche@gmail.com>
2022-10-21 09:16:14 +0100

    list as component

--- src/App.jsx
+++ src/App.jsx
@@ -29,22 +29,29 @@ function App() {
       <input id="search" type="text" />
 
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              {/* Comment section */}
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+
+      <List />
     </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            {/* Comment section */}
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
