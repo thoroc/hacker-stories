287d5d2538fa2f2e76e55900ae288cc2a8d69ab9
Thomas Roche <thomas.a.roche@gmail.com>
2022-10-21 09:02:25 +0100

    welcome as object

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import './App.css';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
