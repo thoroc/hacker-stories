0727f4483ea9e51af90eecf63469ef35453d1c33
Thomas Roche <thomas.a.roche@gmail.com>
2022-10-21 09:01:05 +0100

    added simple search box

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ const title = 'React';
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
