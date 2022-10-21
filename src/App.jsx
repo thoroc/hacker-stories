bfecb7d4ce541d94e59a34f36728ec333e52ba8f
Thomas Roche <thomas.a.roche@gmail.com>
2022-10-21 13:53:06 +0100

    callback handler through props

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,59 @@
 import * as React from 'react';
 import './App.css';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Material-UI',
-    url: 'https://mui.com/material-ui/',
-    author: 'Olivier Tassinari',
-    num_comments: 5,
-    points: 11,
-    objectID: 2,
-  },
-  {
-    title: 'Jest',
-    url: 'https://jestjs.io/',
-    author: 'Simen Bekkhus',
-    num_comments: 2,
-    points: 1,
-    objectID: 3,
-  },
-  {
-    title: 'Babel',
-    url: 'https://babeljs.io/',
-    author: 'Sebastian ?',
-    num_comments: 1,
-    points: 1,
-    objectID: 4,
-  },
-];
-
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'Material-UI',
+      url: 'https://mui.com/material-ui/',
+      author: 'Olivier Tassinari',
+      num_comments: 5,
+      points: 11,
+      objectID: 2,
+    },
+    {
+      title: 'Jest',
+      url: 'https://jestjs.io/',
+      author: 'Simen Bekkhus',
+      num_comments: 2,
+      points: 1,
+      objectID: 3,
+    },
+    {
+      title: 'Babel',
+      url: 'https://babeljs.io/',
+      author: 'Sebastian ?',
+      num_comments: 1,
+      points: 1,
+      objectID: 4,
+    },
+  ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
@@ -58,12 +62,14 @@ const App = () => {
   );
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
     console.log('Searching for: ' + searchTerm);
+
+    props.onSearch(event);
   };
 
   return (
